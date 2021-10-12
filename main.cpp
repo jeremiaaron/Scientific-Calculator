d18266114f7c49f6a6462e019550746ab2815ebb
jeremiaaron <jeremiah.aaron117@gmail.com>
2021-10-12 22:01:21 +0700

    New operators, main menu, history, classes moved
    - Added trigonometric and advanced operators (sin, cos, tan, sqrt, ^, log, ln)
    - Added main menu
    - Vals and Ops classes with their functions are defined in Lists.h and implemented in Lists.cpp
    - Included HistoryCalc.h and Lists.h
    -

--- main.cpp
+++ main.cpp
@@ -1,118 +1,40 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <cctype>
 #include <cmath>
-#include <iomanip>
+#include "Lists.h"
+#include "HistoryCalc.h"
 using namespace std;
 
-class Vals {
-    public:
-        float data;
-        Vals* next;
-
-        Vals(float x) {
-            data = x;
-            next = NULL;
-        }
-};
-
-class Ops {
-    public:
-        char data;
-        Ops* next;
-
-        Ops(char c) {
-            data = c;
-            next = NULL;
-        }
-};
-
-void pushToTailVal(Vals** headRef, Vals** tailRef, float newData) {
-    Vals* newNode = new Vals(newData);
-
-    if(*headRef == NULL) 
-        *headRef = newNode;
-    else
-        (*tailRef)->next = newNode;
-
-    (*tailRef) = newNode;
-}
-
-void pushToTailOp(Ops** headRef, Ops** tailRef, char newData) {
-    Ops* newNode = new Ops(newData);
-
-    if(*headRef == NULL) 
-        *headRef = newNode;
-    else
-        (*tailRef)->next = newNode;
-
-    (*tailRef) = newNode;
-}
-
-void deleteTailVal(Vals** headRef, Vals** tailRef) {
-    if(*headRef == NULL)
-        return;
-    
-    if((*headRef)->next == NULL) {
-        (*headRef) = (*headRef)->next;
-        free(*headRef);
-        return;
-    }
-
-    Vals* temp = (*headRef);
-    while(temp->next->next != NULL) {
-        temp = temp->next;
-    }
-
-    free(temp->next);
-
-    temp->next = NULL;
-
-    (*tailRef) = temp;
-}
-
-void deleteTailOp(Ops** headRef, Ops** tailRef) {
-    if(*headRef == NULL)
-        return;
-    
-    if((*headRef)->next == NULL) {
-        (*headRef) = (*headRef)->next;
-        free(*headRef);
-        return;
-    }
-
-    Ops* temp = (*headRef);
-    while(temp->next->next != NULL) {
-        temp = temp->next;
-    }
-
-    free(temp->next);
-
-    temp->next = NULL;
-
-    (*tailRef) = temp;
-}
-
-void printListVal(Vals* valsHead) {
-    while(valsHead != NULL) {
-        cout << valsHead->data << " ";
-        valsHead = valsHead->next;
-    }
-}
-
-void printListOp(Ops* opsHead) {
-    while(opsHead != NULL) {
-        cout << opsHead->data << " ";
-        opsHead = opsHead->next;
-    }
-}
-
 //--------------------------------------------------------------------------------------
+//FUNCTIONS FOR EXPRESSION EVALUATION (+, -, *, /, ^, sin, cos, tan, sqrt, log, ln)
 
-float opCalculate(float x, float y, char op) {
-    if(op == '+') return ((float)x + (float)y);
-    if(op == '-') return ((float)x - (float)y);
-    if(op == '*') return ((float)x * (float)y);
-    if(op == '/') return ((float)x / (float)y);
+float basicOpCalculate(float x, float y, char op) {
+    if(op == '+') return (x + y);
+    if(op == '-') return (x - y);
+    if(op == '*') return (x * y);
+    if(op == '/') return (x / y);
+    if(op == '^') return pow(x, y);
+    return 0;
+}
+
+float advOpCalculate(float x, char op) {
+    
+    /*
+    CHAR REPRESENTATIONS FOR TRIGONOMETRIC AND ADVANCED FUNCTIONS
+    's': sin
+    'c': cos
+    't': tan
+    'q': sqrt
+    'l': log
+    'e': ln
+    */
+
+    if(op == 's') return sin(x);
+    if(op == 'c') return cos(x);
+    if(op == 't') return tan(x);
+    if(op == 'q') return sqrt(x);
+    if(op == 'l') return log10(x);
+    if(op == 'e') return log(x);
     return 0;
 }
 
@@ -120,19 +42,58 @@ int opPrecedence(char op) {
     if(op == '+' || op == '-')
         return 1;
     else if (op == '*' || op == '/')
-        return 2;
+        return 2; 
+    else if (op == 's' || op == 'c' || op == 't' || op == 'q' || op == 'l' || op == 'e')
+        return 3;
+    else if (op == '^')
+        return 4;  
     else
         return 0;
 }
 
 float inputResult(string input, Vals* valsHead, Vals* valsTail, Ops* opsHead, Ops* opsTail) {
     for(int i = 0; i < input.length(); i++) {
-        if(input[i] == ' ' || input[i] == '\n') {
+        if(input[i] == ' ') {
             continue;
         }
 
         else if(input[i] == '(') {
-            pushToTailOp(&opsHead, &opsTail, input[i]);
+            opsHead->pushToTail(&opsHead, &opsTail, input[i]);
+        }
+
+        else if(isalpha(input[i])) {
+            if(input[i] == 's') {
+                if(i < input.length() - 2 && input[i + 1] == 'i' && input[i + 2] == 'n') {
+                    opsHead->pushToTail(&opsHead, &opsTail, 's');
+                    i += 2;
+                }
+                else if(i < input.length() - 3 && input[i + 1] == 'q' && input[i + 2] == 'r' && input[i + 3] == 't') {
+                    opsHead->pushToTail(&opsHead, &opsTail, 'q');
+                    i += 3;
+                }
+            }
+
+            else if(i < input.length() - 2 && input[i] == 'c' && input[i + 1] == 'o' && input[i + 2] == 's') {
+                opsHead->pushToTail(&opsHead, &opsTail, 'c');
+                i += 2;
+            }
+
+            else if(i < input.length() - 2 && input[i] == 't' && input[i + 1] == 'a' && input[i + 2] == 'n') {
+                opsHead->pushToTail(&opsHead, &opsTail, 't');
+                i += 2;
+            }
+
+            else if(input[i] == 'l') {
+                if(i < input.length() - 2 && input[i + 1] == 'o' && input[i + 2] == 'g') {
+                    opsHead->pushToTail(&opsHead, &opsTail, 'l');
+                    i += 2;
+                }
+
+                else if(i < input.length() - 1 && input[i + 1] == 'n') {
+                    opsHead->pushToTail(&opsHead, &opsTail, 'e');
+                    i++;
+                }
+            }
         }
 
         else if(isdigit(input[i])) {
@@ -143,71 +104,216 @@ float inputResult(string input, Vals* valsHead, Vals* valsTail, Ops* opsHead, Op
                 i++;
             }
 
-            pushToTailVal(&valsHead, &valsTail, temp);
+            valsHead->pushToTail(&valsHead, &valsTail, temp);
             i--;
         }
 
         else if(input[i] == ')') {
             while(opsHead != NULL && opsTail->data != '(') {
-                float b = valsTail->data;
-                deleteTailVal(&valsHead, &valsTail);
+                char tailOp = opsTail->data;
 
-                float a = valsTail->data;
-                deleteTailVal(&valsHead, &valsTail);
+                if(tailOp == 's' || tailOp == 'c' || tailOp == 't' || tailOp == 'q' || tailOp == 'l' || tailOp == 'e') {
+                    float x = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
 
-                char op = opsTail->data;
-                deleteTailOp(&opsHead, &opsTail);
+                    char op = opsTail->data;
+                    opsHead->deleteTail(&opsHead, &opsTail);
 
-                float result = opCalculate(a, b, op);
-                pushToTailVal(&valsHead, &valsTail, result);
+                    float result = advOpCalculate(x, op);
+                    valsHead->pushToTail(&valsHead, &valsTail, result);
+                }
+
+                else {
+                    float b = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
+
+                    float a = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
+
+                    char op = opsTail->data;
+                    opsHead->deleteTail(&opsHead, &opsTail);
+
+                    float result = basicOpCalculate(a, b, op);
+                    valsHead->pushToTail(&valsHead, &valsTail, result);
+                }
             }
 
-            if(opsHead != NULL) deleteTailOp(&opsHead, &opsTail);
+            if(opsHead != NULL) opsHead->deleteTail(&opsHead, &opsTail);
         }
         
         else {
             while(opsHead != NULL && opPrecedence(input[i]) <= opPrecedence(opsTail->data)) {
-                float b = valsTail->data;
-                deleteTailVal(&valsHead, &valsTail);
+                char tailOp = opsTail->data;
 
-                float a = valsTail->data;
-                deleteTailVal(&valsHead, &valsTail);
+                if(tailOp == 's' || tailOp == 'c' || tailOp == 't' || tailOp == 'q' || tailOp == 'l' || tailOp == 'e') {
+                    float x = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
 
-                char op = opsTail->data;
-                deleteTailOp(&opsHead, &opsTail);
+                    char op = opsTail->data;
+                    opsHead->deleteTail(&opsHead, &opsTail);
 
-                float result = opCalculate(a, b, op);
-                pushToTailVal(&valsHead, &valsTail, result);
+                    float result = advOpCalculate(x, op);
+                    valsHead->pushToTail(&valsHead, &valsTail, result);
+                }
+
+                else {
+                    float b = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
+
+                    float a = valsTail->data;
+                    valsHead->deleteTail(&valsHead, &valsTail);
+
+                    char op = opsTail->data;
+                    opsHead->deleteTail(&opsHead, &opsTail);
+
+                    float result = basicOpCalculate(a, b, op);
+                    valsHead->pushToTail(&valsHead, &valsTail, result);
+                }
             }
     
-            pushToTailOp(&opsHead, &opsTail, input[i]);
+            opsHead->pushToTail(&opsHead, &opsTail, input[i]);
         }
     }
 
     while(opsHead != NULL) {
-        float b = valsTail->data;
-        deleteTailVal(&valsHead, &valsTail);
+        char tailOp = opsTail->data;
 
-        float a = valsTail->data;
-        deleteTailVal(&valsHead, &valsTail);
+        if(tailOp == 's' || tailOp == 'c' || tailOp == 't' || tailOp == 'q' || tailOp == 'l' || tailOp == 'e') {
+            float x = valsTail->data;
+            valsHead->deleteTail(&valsHead, &valsTail);
 
-        char op = opsTail->data;
-        deleteTailOp(&opsHead, &opsTail);
+            char op = opsTail->data;
+            opsHead->deleteTail(&opsHead, &opsTail);
 
-        float result = opCalculate(a, b, op);
-        pushToTailVal(&valsHead, &valsTail, result);
+            float result = advOpCalculate(x, op);
+            valsHead->pushToTail(&valsHead, &valsTail, result);
+        }
+
+        else {
+            float b = valsTail->data;
+            valsHead->deleteTail(&valsHead, &valsTail);
+
+            float a = valsTail->data;
+            valsHead->deleteTail(&valsHead, &valsTail);
+
+            char op = opsTail->data;
+            opsHead->deleteTail(&opsHead, &opsTail);
+
+            float result = basicOpCalculate(a, b, op);
+            valsHead->pushToTail(&valsHead, &valsTail, result);
+        }
     }
 
     return valsTail->data;
 }
 
+//FUNCTION MAIN MENU
+int mainMenu() {
+    int menu;
+    cout << "=========================" << endl;
+    cout << "\tMAIN MENU\t" << endl;
+    cout << "=========================" << endl << endl;
+    cout << "1. Open scientific calculator" << endl;
+    cout << "2. Open calculator history" << endl;
+    cout << "0. Exit program" << endl << endl;
+    
+    cout << "Enter your option: ";
+    cin >> menu;
+    getchar();
+
+    system("cls");
+
+    return menu;
+}
+
+//DRIVER PROGRAM
 int main() {
     Vals* valsHead = NULL, *valsTail = NULL;
     Ops* opsHead = NULL, *opsTail = NULL;
+    Queue* history = new Queue(1000);
     
-    string input;
-    getline(cin, input);
-    cout << "The result is " << inputResult(input, valsHead, valsTail, opsHead, opsTail) << endl;
+    //obtain value from user input in main menu
+    int menu = mainMenu();
+
+    while(menu != 0) {
+        //SCIENTIFIC CALCULATOR MENU
+        if(menu == 1) {
+            cout << "=====================================" << endl;
+            cout << "\tSCIENTIFIC CALCULATOR\t" << endl;
+            cout << "=====================================" << endl;
+            cout << "Available operators: +, -, *, /, ^, (, ), sin, cos, tan, sqrt, log, ln" << endl;
+            cout << "Please enter your expression(s)! (type DONE to return to main menu)" << endl << endl;
+
+            string input;
+
+            cout << "Enter an expression:" << endl;
+            getline(cin, input);
+            while(input.compare("DONE") != 0) {
+                float result = inputResult(input, valsHead, valsTail, opsHead, opsTail);
+
+                cout << "= " << result << endl << endl;
+                history->enqueue(history, input, result);
+                
+                cout << "Enter an expression:" << endl;
+                getline(cin, input);
+            }
+        }
+
+        //HISTORY MENU
+        else if(menu == 2) {
+            cout << "=====================================" << endl;
+            cout << "\tCALCULATOR HISTORY\t" << endl;
+            cout << "=====================================" << endl;
+            cout << "1. Show history list" << endl;
+            cout << "2. Pick a history to delete" << endl;
+            cout << "3. Delete whole history" << endl;
+            cout << "0. Back to main menu" << endl << endl;
+
+            int option;
+
+            cout << "Enter your option: ";
+            cin >> option;
+            while(option != 0) {
+                if(option == 1) {
+                    history->showHistory(history);
+                }
+
+                else if(option == 2) {
+                    int index;
+                    cout << "Which history do you want to delete? ";
+                    cin >> index;
+                    history->dequeue_At(history, index);
+                }
+
+                else if(option == 3) {
+                    char answer;
+
+                    cout << "Do you want to delete the whole history (Y/N)? ";
+                    cin >> answer;
+
+                    switch(tolower(answer)) {
+                        case 'y': history->dequeue_All(history);
+                                  if(!history->isEmpty(history))
+                                      cout << "All history successfully deleted" << endl;
+                                  break;
+                        case 'n': cout << "History deletion cancelled" << endl; break;
+                    }
+
+                    cout << endl;
+                }
+
+                cout << "Enter your option: ";
+                cin >> option;
+            }
+        }
+
+        system("cls"); //clears current menu before moving to another menu
+
+        menu = mainMenu();
+    }
+
+    //exits program when user enters 0
+    exit(0);
 
     return 0;
 }
